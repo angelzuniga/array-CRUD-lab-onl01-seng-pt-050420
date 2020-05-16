304fa2c4c15279392fa00cf9a3d8a3cfbd2e5e42
Angel Zuniga <angelzuniga@outlook.com>
2020-05-16 19:47:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,9 @@ end
 def add_element_to_end_of_array(array, element)
   array << element
 end
-arary.
+
 def add_element_to_start_of_array(array, element)
-  
+  arary.unshift("#{element}")
 end
 
 def remove_element_from_end_of_array(array)
